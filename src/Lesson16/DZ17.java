970c8406df339ba48faeb5d63f629cfa18dc5c35
Yumax <yumax83@gmail.com>
2024-12-05 17:09:11 +0300

    DZ17

---
+++ src/Lesson16/DZ17.java
@@ -0,0 +1,16 @@
+package Lesson16;
+
+public class DZ16 {
+    public static void main(String[] args) {
+        String text = " Текст с повторяющимися символами ";
+        text = text.trim().replace(" ", "");
+        StringBuilder sb = new StringBuilder(text);
+        for (int i = 1; i < sb.length(); i++) {
+            String ch = String.valueOf(sb.charAt(i - 1));
+            while (sb.indexOf(ch, i) != -1) {
+               sb.deleteCharAt(sb.indexOf(ch, i));
+            }
+        }
+        System.out.println(sb);
+    }
+}
