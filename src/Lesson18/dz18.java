901d1adda6d9f0c6b5802b1fc364591e30a666cd
Yumax <yumax83@gmail.com>
2024-12-06 18:39:11 +0300

    DZ18

--- src/Lesson18/dz18.java
+++ src/Lesson18/dz18.java
@@ -1,4 +1,36 @@
 package Lesson18;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
 public class dz18 {
+    public static void main(String[] args) {
+        String s = "123456@i.ru,  123_456@ru.name.ru,  login@i.ru,  логин-1@i.ru,  login.3@i.ru,  login.3-67@i.ru,  1login@ru.name.ru";
+        try {
+
+            System.out.println(s);
+            String pattern = "[a-zа-яё0-9_]+([\\.-]*[a-zа-яё0-9_]+)*@[a-z]+([\\.]+[a-z]+)*";
+            /* Сложно для понимания, первый раз столкнулся с такими шаблонами. Шаблон построил, задача решена, отрабатывает правильно.
+            Но с учетом отсутствия опыта применения, не могу убедиться, что все идеально верно. Возможно на данный момент этого не требуется.
+                Формирую шаблон так:
+               [a-zа-яё0-9_]+     буквы анг и рус, цифры, нижнее подчеркивание это обяз 1 вхождение или мнгого раз.
+               ([\\.-]*[a-zа-яё0-9_]+)*  точка или тире могут быть или не быть, но после них обязательно буквы или цифры и все это вместе может быть или не быть
+               @[a-z]+   указание на символ почты @ после чего обяз 1 вхождение анг буквы.
+               ([\\.]+[a-z]+)* после букв должна быть хотя бы одна точка, после чего обяз 1 вхождение буквы, и все это может быть или не быть, и если быть, то быть более одного вхождения
+            */
+            Pattern regex = Pattern.compile(pattern);
+            Matcher matcher = regex.matcher(s);
+
+            while (matcher.find()) {
+                System.out.print(matcher.group() + "   ");
+            }
+        } catch (PatternSyntaxException pse) {
+            System.out.println("Не правильное регулярное выражение: " + pse.getMessage());
+            System.out.println("Описание: " + pse.getDescription());
+            System.out.println("Позиция: " + pse.getIndex());
+            System.out.println("Не правильный шаблон: " + pse.getPattern());
+
+        }
+    }
 }
--- src/Lesson18/urok18.java
+++ src/Lesson18/urok18.java
@@ -1,4 +1,42 @@
 package Lesson18;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
 public class urok18 {
+    public static void main(String[] args) {
+        try {
+            // String s = "Час в 24-часовом формате от 00 до 23ю 2021-06-15Т21:45. " +
+            //      "Минуты в диапозоне от 00 до 59. 2021-0615Т01:09";
+            //  String s = "12 сентября 2024 года 3452`343564";
+           // String s = "+7 499 456-45-78, +74994564578, +7 (499) 456 45 78, 74994564578";
+
+           // String pattern = "[+]?\\d{7,}";
+            //String pattern = "\\+?7\\d{10}";
+          //  String s = "I learning Java Hello";
+            //String pattern = "^\\w+\\s\\w+"; //только от начала
+           // String pattern = "\\w+\\s\\w+"; // любые
+          //  String pattern = "\\w+\\s\\w+$"; // в конце
+
+            // String pattern = "\\d{2,4}"
+            // String pattern = "\\d{2,}";
+
+            String s = "Java_master";
+            String pattern = "^[\\w-]{3,16}$"; //  [A-Za-z0-9-] (от 3 до 16 символов)
+
+            Pattern regex = Pattern.compile(pattern);
+            Matcher matcher = regex.matcher(s);
+
+            while (matcher.find()) {
+                System.out.println(matcher.group());
+            }
+        } catch (PatternSyntaxException pse) {
+            System.out.println("Не правильное регулярное выражение: " + pse.getMessage());
+            System.out.println("Опимание: " + pse.getDescription());
+            System.out.println("Позиция: " + pse.getIndex());
+            System.out.println("Не правильный шаблон: " + pse.getPattern());
+
+        }
+    }
 }
