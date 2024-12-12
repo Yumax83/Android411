51f67b79f9d11ec09b235385ebd93951b659261f
Yumax <yumax83@gmail.com>
2024-12-12 10:17:44 +0300

    rect

--- src/rect/Main.java
+++ src/rect/Main.java
@@ -1,4 +1,18 @@
 package rect;
 
 public class Main {
+    public static void main(String[] args) {
+        Rectangle rect1 = new Rectangle(4, 15);
+        rect1.setLength(9);
+        rect1.setWidth(3);
+        System.out.println("Длина прямоугольника: " + rect1.getLength());
+        System.out.println("Ширина прямоугольника: " + rect1.getWidth());
+        System.out.println("Площадь прямоугольника: " + rect1.getArea());
+        System.out.println("Периметр прямоугольника: " + rect1.getPerimetr());
+        System.out.printf("Гипотенуза прямоугольника: %.2f%n", rect1.getHypotenuse());
+        System.out.printf("Фигура прямоугольника: %n");
+        rect1.getFiger();
+
+    }
+
 }
--- src/rect/Rectangle.java
+++ src/rect/Rectangle.java
@@ -1,4 +1,53 @@
 package rect;
 
 public class Rectangle {
+    private int length;
+    private int width;
+
+    public Rectangle(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setLength(int length) {
+        if (length > 0)
+            this.length = length;
+    }
+
+    public void setWidth(int width) {
+        if (width > 0)
+            this.width = width;
+    }
+
+    public int getArea() {
+        return length * width;
+    }
+
+    public int getPerimetr() {
+        return 2 * (length * width);
+    }
+
+    public double getHypotenuse() {
+        return Math.sqrt((Math.pow(length, 2) + Math.pow(width, 2)));
+    }
+
+    public void getFiger() {
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < length; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+
+
 }
