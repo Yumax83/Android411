98bca26467dba4b35d403d62d4086af62fd494c5
Yumax <yumax83@gmail.com>
2025-01-28 00:06:27 +0300

    update DZ31 DZ32

--- src/lesson31/DZ31/CheckEmpty.java
+++ src/lesson31/DZ31/CheckEmpty.java
@@ -1,4 +1,16 @@
 package lesson31.DZ31;
 
+import java.util.function.Predicate;
+
 public class CheckEmpty {
+    public static void main(String[] args) {
+        Predicate<String> checkingEmpty = str -> str.trim().isEmpty(); //проверка пустой строки с удалением пробелов
+
+        String stroka1 = "     ";
+        String stroka2 = "Строка содержит текст!";
+
+        System.out.println("Строка 1: " + stroka1 + "\nСтрока 1 пустая: " + checkingEmpty.test(stroka1));
+        System.out.println("\nСтрока 2: " + stroka2 + "\nСтрока содержит текст: " + checkingEmpty.test(stroka2));
+
+    }
 }
